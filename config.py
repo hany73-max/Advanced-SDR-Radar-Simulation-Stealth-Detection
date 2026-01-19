c256f615ffa0a93dee0c2ce419b9a139558a1412
Mostafa Hatem <hatmmstfy536@gmail.com>
2026-01-19 04:14:42 +0200

    Refactor radar configuration parameters in config.py

--- config.py
+++ config.py
@@ -1,8 +1,19 @@
-# SkyGuard Radar Global Configuration
-C = 3e8              # Speed of light (m/s)
-R_MAX = 150000       # Maximum Range 150 KM
-FC = 10e9            # Carrier Frequency 10GHz (X-band)
-B = 150e6            # Bandwidth 150MHz
-TC = 2e-3            # Chirp Duration 2ms
-FS = 1e6             # Initial Sampling Rate (1MHz)
-STEALTH_RCS = 0.01   # RCS for Stealth Target (m^2)
+import numpy as np
+
+# --- Radar Hardware & Waveform ---
+C = 3e8                      # Speed of light (m/s)
+FC = 10e9                    # 10 GHz (X-band)
+LAMBDA = C / FC              # Wavelength (0.03 meters)
+B = 150e6                    # Bandwidth (150 MHz)
+TC = 2e-3                    # Chirp time (2ms)
+SLOPE = B / TC               # Chirp slope (Hz/s)
+
+# --- Sampling & Resolution ---
+FS = 2.5e6                   # Sampling rate (2.5 MHz)
+N_SAMPLES = int(FS * TC)     # Number of samples per chirp (5000 samples)
+R_MAX = 150000               # Max range (150 KM)
+RANGE_RES = C / (2 * B)      # Range resolution (1.0 meter)
+
+# --- Target Specs ---
+MIN_RCS = 0.01               # Stealth target RCS (m^2)
+BOLTZMANN = 1.38e-23         # Boltzmann constant
